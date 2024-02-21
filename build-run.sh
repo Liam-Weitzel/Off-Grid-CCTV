7400ff4222006e8606652d9c2cb0c3e53ffcbd1b
Liam-Weitzel <liam.weitzel@gmail.com>
2024-02-21 17:45:50 +0000

    Add infrastructure for live stream object detection
    Add a docker image that installs and builds node.js version 8, opencv version 3.3.1, and ffmpeg stable. This is done for the necessary dependancies to run: https://github.com/bookjan/live-stream-object-detection/tree/master

--- build-run.sh
+++ build-run.sh
@@ -5,39 +5,54 @@ declare -a imageName=()
 declare -a imageVersion=()
 size=0
 
+echo "The following commands will be executed:"
+
 for dir in */
 do
     currentDir=$(echo "${dir}" | sed 's/\///g')
     imageName[${size}]=${currentDir}
     #echo "${currentDir}"
+    if find "${currentDir}" -type f -name "Dockerfile" | grep -q .; then
 
-    #fetch the latest version number of each image
-    fullVersion=$(sudo docker images | grep -o -P "${currentDir}.{0,20}" | grep -o -P ' v.{0,5}' | sed 's/ //g' | sed 's/v//g' | sort -V | tail -n 1)
-    majorVersion=$(echo "${fullVersion}" | grep -o -P '.{0,10}\.' | sed 's/\.//g')
-    minorVersion=$(echo "${fullVersion}" | grep -o -P '\..{0,10}' | sed 's/\.//g')
+	#fetch the latest version number of each image
+	fullVersion=$(sudo docker images | grep -o -P "${currentDir}.{0,20}" | grep -o -P ' v.{0,5}' | sed 's/ //g' | sed 's/v//g' | sort -V | tail -n 1)
+	majorVersion=$(echo "${fullVersion}" | grep -o -P '.{0,10}\.' | sed 's/\.//g')
+	minorVersion=$(echo "${fullVersion}" | grep -o -P '\..{0,10}' | sed 's/\.//g')
 
-    #echo "${currentDir}: ${fullVersion}  -  v${majorVersion}.${minorVersion}"
+	#echo "${currentDir}: ${fullVersion}  -  v${majorVersion}.${minorVersion}"
 
-    #increment version no.
-    if [ $1 = 'major' ]; then
-	((majorVersion++))
-    elif [ $1 = 'minor' ]; then
-	((minorVersion++))
+	#increment version no.
+	if [ $1 = 'major' ]; then
+	    ((majorVersion++))
+	elif [ $1 = 'minor' ]; then
+	    ((minorVersion++))
+	fi
+	newVersion=$(echo "v${majorVersion}.${minorVersion}")
+
+	imageVersion[${size}]=${newVersion}
+	#echo "${newVersion}"
+	
+	echo "cd ${currentDir}/ && sudo docker build . -t ${currentDir}:${newVersion} -t ${currentDir}:latest && cd ../"
+
+	((size++))
     fi
-    newVersion=$(echo "v${majorVersion}.${minorVersion}")
+done
 
-    imageVersion[${size}]=${newVersion}
-    #echo "${newVersion}"
-
-    ((size++))
+read -t 1 -n 1
+echo "Press any key to continue"
+while [ true ] ; do
+read -t 3 -n 1
+if [ $? = 0 ] ; then
+break ;
+else
+continue ;
+fi
 done
 
 for (( i = 0; i <= $size; i++ ))
 do
     #echo "${imageName[$i]}  ${imageVersion[$i]}"
-    if find "${imageName[$i]}" -type f -name "Dockerfile" | grep -q .; then
-      cd ${imageName[$i]}/ && sudo docker build . -t ${imageName[$i]}:${imageVersion[$i]} -t ${imageName[$i]}:latest && cd ../
-    fi
+    cd ${imageName[$i]}/ && sudo docker build . -t ${imageName[$i]}:${imageVersion[$i]} -t ${imageName[$i]}:latest && cd ../
 done
 
 sudo docker-compose up -d #runs docker compose headless mode
