0d54afd0400b70bbe6f93c37db789703b8f31caa
Liam-Weitzel <liam.weitzel@gmail.com>
2024-03-01 15:20:55 +0000

    MVP first camera stream to react frontend

--- web/src/App.js
+++ web/src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import JsmpegPlayer from './JsmpegPlayer';
 import './App.css';
 
 function App() {
+  let jsmpegPlayer = null;
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <JsmpegPlayer
+          wrapperClassName="video-wrapper"
+          videoUrl="ws://192.168.92.22:8081" //get using API!!
+          onRef={ref => jsmpegPlayer = ref}
+        />
+        <JsmpegPlayer
+          wrapperClassName="video-wrapper"
+          videoUrl="ws://192.168.92.22:8084"
+          onRef={ref => jsmpegPlayer = ref}
+        />
       </header>
     </div>
   );
