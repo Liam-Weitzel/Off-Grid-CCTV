2edb028e294e064b80b682594bb12d27b5869543
Liam-Weitzel <liam.weitzel@gmail.com>
2024-03-05 20:55:18 +0000

    CSS rework :(

--- web/src/components/controlPanel.js
+++ web/src/components/controlPanel.js
@@ -25,10 +25,10 @@ function ControlPanel(props) {
 
   return (
     <div>
-    <div className="control-panel">
-      <button onClick={addCamera}> + </button>
-      <button onClick={removeCamera}> - </button>
-      <button onClick={refreshCamera}> r </button>
+    <div className="control-panel mapboxgl-ctrl mapboxgl-ctrl-group-horizontal">
+      <button className="add-camera-icon" type="button" aria-label="Add camera" aria-disabled="false" onClick={addCamera}><span className="mapboxgl-ctrl-icon" aria-hidden="true" title="Add camera"></span></button>
+      <button className="remove-camera-icon" type="button" aria-label="Remove camera" aria-disabled="false" onClick={removeCamera}><span className="mapboxgl-ctrl-icon" aria-hidden="true" title="Remove camera"></span></button>
+      <button className="refresh-camera-icon" type="button" aria-label="Refresh camera" aria-disabled="false" onClick={refreshCamera}><span className="mapboxgl-ctrl-icon" aria-hidden="true" title="Refresh camera"></span></button>
     </div>
     {(addCameraPopUpVisible || removeCameraPopUpVisible || refreshCameraPopUpVisible) && (
     <div className="control-panel action-popup">
