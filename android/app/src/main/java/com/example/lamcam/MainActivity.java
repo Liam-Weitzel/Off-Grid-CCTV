894ac05e799bdf841d726baa20d713ed4da1130e
Liam-Weitzel <liam.weitzel@gmail.com>
2024-03-19 09:22:42 +0000

    Get one live stream to android app using a web view

--- android/app/src/main/java/com/example/lamcam/MainActivity.java
+++ android/app/src/main/java/com/example/lamcam/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.lamcam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView wv = (WebView) findViewById(R.id.webView);
+        wv.setWebViewClient(new WebViewClient());
+        String URL = "http://192.168.43.158:8080/viewCamera?url=ws://192.168.43.158:8081/";
+        wv.loadUrl(String.valueOf(Uri.parse(URL)));
+        WebSettings webSettings = wv.getSettings();
+        webSettings.setJavaScriptEnabled(true);
     }
 }
