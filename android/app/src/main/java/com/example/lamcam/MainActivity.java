42451a3a936273175009abe796aa3f118cd7e75e
Liam-Weitzel <liam.weitzel@gmail.com>
2024-03-21 09:57:50 +0000

    create map view using mapbox API

--- android/app/src/main/java/com/example/lamcam/MainActivity.java
+++ android/app/src/main/java/com/example/lamcam/MainActivity.java
@@ -1,18 +1,32 @@
 package com.example.lamcam;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.net.Uri;
-import android.os.Bundle;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import com.mapbox.geojson.Point;
+import com.mapbox.maps.CameraOptions;
+import com.mapbox.maps.MapView;
 
 public class MainActivity extends AppCompatActivity {
+    private MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mapView = new MapView(this);
+        mapView.getMapboxMap().setCamera(
+                new CameraOptions.Builder()
+                        .center(Point.fromLngLat(-98.0, 39.5))
+                        .pitch(0.0)
+                        .zoom(2.0)
+                        .bearing(0.0)
+                        .build()
+        );
+        setContentView(mapView);
+
+        /*
         setContentView(R.layout.activity_main);
 
         WebView wv = (WebView) findViewById(R.id.webView);
@@ -21,5 +35,6 @@ public class MainActivity extends AppCompatActivity {
         wv.loadUrl(String.valueOf(Uri.parse(URL)));
         WebSettings webSettings = wv.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        */
     }
 }
