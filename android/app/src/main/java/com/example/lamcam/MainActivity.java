35a52fad93730c7beb732273deaa5616454a11e1
Liam-Weitzel <liam.weitzel@gmail.com>
2024-03-22 09:28:04 +0000

    Split mainActivity into three activities
    Also make sure aspect ratio is always maintained in cameraView

--- android/app/src/main/java/com/example/lamcam/MainActivity.java
+++ android/app/src/main/java/com/example/lamcam/MainActivity.java
@@ -1,130 +1,22 @@
 package com.example.lamcam;
 
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
+import android.content.Intent;
 import android.os.Bundle;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.Toast;
+import android.view.View;
 
-import androidx.annotation.DrawableRes;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.content.res.AppCompatResources;
-
-import com.mapbox.geojson.Point;
-import com.mapbox.maps.CameraBoundsOptions;
-import com.mapbox.maps.CameraOptions;
-import com.mapbox.maps.CoordinateBounds;
-import com.mapbox.maps.MapView;
-import com.mapbox.maps.MapboxMap;
-import com.mapbox.maps.Style;
-import com.mapbox.maps.extension.style.StyleContract;
-import com.mapbox.maps.extension.style.StyleExtensionImpl;
-import com.mapbox.maps.extension.style.sources.generated.RasterDemSource;
-import com.mapbox.maps.extension.style.terrain.generated.Terrain;
-import com.mapbox.maps.plugin.Plugin;
-import com.mapbox.maps.plugin.annotation.AnnotationConfig;
-import com.mapbox.maps.plugin.annotation.AnnotationPlugin;
-import com.mapbox.maps.plugin.annotation.AnnotationType;
-import com.mapbox.maps.plugin.annotation.generated.PointAnnotationManager;
-import com.mapbox.maps.plugin.annotation.generated.PointAnnotationOptions;
 
 public class MainActivity extends AppCompatActivity {
-    private MapView mapView;
-    private MapboxMap mapboxMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mapView = findViewById(R.id.mapView);
-        mapboxMap = mapView.getMapboxMap();
-
-        mapboxMap.setCamera(
-                new CameraOptions.Builder()
-                        .center(Point.fromLngLat(15.013785520105046, 36.90453150945084))
-                        .pitch(0.0)
-                        .zoom(16.0)
-                        .bearing(-50.0)
-                        .build()
-        );
-
-        CameraBoundsOptions options = new CameraBoundsOptions.Builder()
-                .bounds(CoordinateBounds.singleton(Point.fromLngLat(15.013785520105046, 36.90453150945084)))
-                .maxPitch(60.0)
-                .minZoom(14.0)
-                .maxZoom(17.0)
-                .build();
-        mapboxMap.setBounds(options);
-        mapboxMap.loadStyle(createStyle());
-        addAnnotationToMap();
-
-        WebView wv = (WebView) findViewById(R.id.webView);
-        wv.setWebViewClient(new WebViewClient());
-        String URL = "http://192.168.43.158:8080/viewCamera?url=ws://192.168.43.158:8081/";
-        wv.loadUrl(String.valueOf(Uri.parse(URL)));
-        WebSettings webSettings = wv.getSettings();
-        webSettings.setJavaScriptEnabled(true);
     }
 
-    private StyleContract.StyleExtension createStyle() {
-        StyleExtensionImpl.Builder builder = new StyleExtensionImpl.Builder(Style.SATELLITE);
-
-        RasterDemSource rasterDemSource = new RasterDemSource(new RasterDemSource.Builder("TERRAIN_SOURCE").tileSize(514));
-        rasterDemSource.url("mapbox://mapbox.mapbox-terrain-dem-v1");
-        builder.addSource(rasterDemSource);
-
-        Terrain terrain = new Terrain("TERRAIN_SOURCE");
-        terrain.exaggeration(2.5);
-        builder.setTerrain(terrain);
-
-        return builder.build();
+    public void button1clicked(View view) {
+        Intent intent = new Intent(this, view_map.class);
+        startActivity(intent);
     }
-
-    private void addAnnotationToMap() {
-        Bitmap bitmap = bitmapFromDrawableRes(this, R.drawable.camera);
-        if (bitmap != null) {
-            AnnotationPlugin annotationApi = mapView.getPlugin(Plugin.MAPBOX_ANNOTATION_PLUGIN_ID);
-            PointAnnotationManager pointAnnotationManager = (PointAnnotationManager) annotationApi.createAnnotationManager(AnnotationType.PointAnnotation, new AnnotationConfig());
-            PointAnnotationOptions pointAnnotationOptions = new PointAnnotationOptions()
-                    .withPoint(Point.fromLngLat(15.013785520105046, 36.90453150945084))
-                    .withIconImage(bitmap);
-            pointAnnotationManager.create(pointAnnotationOptions);
-
-            pointAnnotationManager.addClickListener(pointAnnotation -> {
-                Toast.makeText(MainActivity.this, "id: " + pointAnnotation.getId(), Toast.LENGTH_LONG).show();
-                return false;
-            });
-        }
-    }
-
-    private Bitmap bitmapFromDrawableRes(Context context, @DrawableRes int resourceId) {
-        return convertDrawableToBitmap(AppCompatResources.getDrawable(context, resourceId));
-    }
-
-    private Bitmap convertDrawableToBitmap(Drawable sourceDrawable) {
-        if (sourceDrawable == null) {
-            return null;
-        }
-        if (sourceDrawable instanceof BitmapDrawable) {
-            return ((BitmapDrawable) sourceDrawable).getBitmap();
-        } else {
-            Drawable drawable = sourceDrawable.getConstantState().newDrawable().mutate();
-            Bitmap bitmap = Bitmap.createBitmap(
-                    drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(),
-                    Bitmap.Config.ARGB_8888
-            );
-            Canvas canvas = new Canvas(bitmap);
-            drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
-            drawable.draw(canvas);
-            return bitmap;
-        }
-    }
-
 }
---
+++ android/app/src/main/java/com/example/lamcam/view_camera.java
@@ -0,0 +1,26 @@
+package com.example.lamcam;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class view_camera extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_camera);
+        Intent intent = getIntent();
+
+        WebView wv = (WebView) findViewById(R.id.webView);
+        wv.setWebViewClient(new WebViewClient());
+        String URL = "http://" + intent.getStringExtra("serverip") + ":" + intent.getStringExtra("apiport") + "/viewCamera?url=ws://" + intent.getStringExtra("serverip") + ":" + intent.getStringExtra("wsport") + "/";;
+        wv.loadUrl(URL);
+        WebSettings webSettings = wv.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+}
