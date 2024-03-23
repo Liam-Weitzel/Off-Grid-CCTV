f5fdfa00c5d3fc9f13049a69ba80148691865d24
Liam-Weitzel <liam.weitzel@gmail.com>
2024-03-23 05:09:53 +0000

    use getSharedPreferences to implement 'remember' checkbox

--- android/app/src/main/java/com/example/lamcam/MainActivity.java
+++ android/app/src/main/java/com/example/lamcam/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.lamcam;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,17 +12,32 @@ import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
     private TextInputLayout serverIp;
+    private CheckBox rememberIp;
+    boolean isRemember;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         serverIp = findViewById(R.id.serverIpInput);
+        SharedPreferences pref = getSharedPreferences("config", MODE_PRIVATE);
+        isRemember = pref.getBoolean("remember",false);
+        rememberIp = findViewById(R.id.rememberIp);
+        rememberIp.setChecked(isRemember);
+
+        if(isRemember) {
+            serverIp.getEditText().setText(pref.getString("serverIp",""));
+        }
     }
 
     public void connectButtonClickHandler(View view) {
+        SharedPreferences pref = getSharedPreferences("config", MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
         Intent intent = new Intent(this, view_map.class);
         intent.putExtra("serverIp", serverIp.getEditText().getText().toString());
+        editor.putBoolean("remember", rememberIp.isChecked());
+        if(rememberIp.isChecked()) editor.putString("serverIp", serverIp.getEditText().getText().toString());
+        editor.commit();
         startActivity(intent);
     }
 }
