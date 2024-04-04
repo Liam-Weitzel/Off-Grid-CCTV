f7946f1e3b2c43bf128e3ab493406a20923f96c2
Liam-Weitzel <liam.weitzel@gmail.com>
2024-04-04 11:01:31 +0100

    Add gathered and labeled data
    I recorded public analog cctv camera streams for 24 hours, every 30 seconds. As a POC, selected one of the cameras, labeled a subset of 300 images and trained for 1 epoch to test

--- backend/ai/model_training.py
+++ backend/ai/model_training.py
@@ -2,15 +2,22 @@ from ultralytics import YOLO
 
 # To train
 model = YOLO('yolov8n.pt')
-model.train(data='data/data.yaml', epochs=1, imgsz=640)
+model.train(data='data/labeled/duomo/data.yaml', epochs=1, imgsz=640)
 
-# # To run trained model
-# model = YOLO('yolov8n.pt').load('./best.pt')
-# results = model(['test1.jpg', 'test2.jpg'])
-# for result in results:
-#     boxes = result.boxes
-#     masks = result.masks
-#     keypoints = result.keypoints
-#     probs = result.probs
-#     result.show()
-#     result.save(filename='result-'+result.path)
+# To run trained model
+model = YOLO('yolov8n.pt').load('./runs/detect/train3/weights/best.pt')
+results = model(['test1.jpg', 'test2.jpg'])
+for result in results:
+    boxes = result.boxes
+    masks = result.masks
+    keypoints = result.keypoints
+    probs = result.probs
+    result.show()
+    result.save(filename='result-'+result.path)
+
+# Validate trained model
+metrics = model.val(data='data/labeled/duomo/data.yaml', imgsz=640)
+metrics.box.map    # map50-95
+metrics.box.map50  # map50
+metrics.box.map75  # map75
+metrics.box.maps   # a list contains map50-95 of each category
