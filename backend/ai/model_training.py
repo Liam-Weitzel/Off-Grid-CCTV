ba9322f2f065e2dec84472729a93d26099ac07d0
Liam-Weitzel <liam.weitzel@gmail.com>
2024-04-06 12:04:04 +0100

    Add hadji_dimitar_square labeled images & change folder structure
    Also add code to run model on live stream directly in python filed

--- backend/ai/model_training.py
+++ backend/ai/model_training.py
@@ -1,23 +1,42 @@
 from ultralytics import YOLO
 
-# To train
-model = YOLO('yolov8n.pt')
-model.train(data='data/labeled/duomo/data.yaml', epochs=1, imgsz=640)
+# # Train
+# model = YOLO('yolov8n.pt')
+# model.train(data='data/duomo/data.yaml', epochs=200, imgsz=640)
 
-# To run trained model
-model = YOLO('yolov8n.pt').load('./runs/detect/train3/weights/best.pt')
-results = model(['test1.jpg', 'test2.jpg'])
-for result in results:
-    boxes = result.boxes
-    masks = result.masks
-    keypoints = result.keypoints
-    probs = result.probs
-    result.show()
-    result.save(filename='result-'+result.path)
+# # Run trained model on images
+# model = YOLO('./saved_runs/doumo_200_epochs/weights/best.pt')#.load('')
+# results = model(['test1.jpg', 'test2.jpg'])
+# for result in results:
+#     boxes = result.boxes
+#     masks = result.masks
+#     keypoints = result.keypoints
+#     probs = result.probs
+#     result.show()
+#     result.save(filename='result-'+result.path)
 
-# Validate trained model
-metrics = model.val(data='data/labeled/duomo/data.yaml', imgsz=640)
-metrics.box.map    # map50-95
-metrics.box.map50  # map50
-metrics.box.map75  # map75
-metrics.box.maps   # a list contains map50-95 of each category
+# # Validate trained model
+# model = YOLO('yolov8n.pt')#.load('')
+# metrics = model.val(data='data/labeled/duomo/data.yaml', imgsz=640)
+# metrics.box.map    # map50-95
+# metrics.box.map50  # map50
+# metrics.box.map75  # map75
+# metrics.box.maps   # a list contains map50-95 of each category
+
+# Run model on live stream
+import cv2
+cap = cv2.VideoCapture("http://88.53.197.250/mjpg/video.mjpg")
+while True:
+    ret, frame = cap.read()
+    model = YOLO('./saved_runs/doumo_200_epochs/weights/best.pt')#.load('')
+    detections = model(frame)[0]
+    for data in detections.boxes.data.tolist():
+        confidence = data[4]
+        if float(confidence) < 0.2:
+            continue
+        xmin, ymin, xmax, ymax = int(data[0]), int(data[1]), int(data[2]), int(data[3])
+        cv2.rectangle(frame, (xmin, ymin) , (xmax, ymax), (255, 0, 0), 2)
+    cv2.imshow('video', frame)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27: 
+        break
