281c70a6ff5ab14527cf950ef787994ebeb26796
Liam-Weitzel <liam.weitzel@gmail.com>
2024-04-25 15:40:52 +0100

    Finalized data collection

--- backend/ai/train.py
+++ backend/ai/train.py
@@ -3,6 +3,185 @@ import fasterrcnn_resnet50_fpn
 import maskrcnn_resnet50_fpn
 import retinanet_resnet50_fpn
 
+model = YOLO('yolov8n.pt')
+model.tune(data='./data/duomo/data.yaml', epochs=30, iterations=300, optimizer='AdamW', plots=False, save=False, val=False)
+
+model = YOLO('yolov8n.pt')
+model.tune(data='./data/hadji_dimitar_square/data.yaml', epochs=30, iterations=300, optimizer='AdamW', plots=False, save=False, val=False)
+
+model = YOLO('yolov8n.pt')
+model.tune(data='./data/keskvaljak/data.yaml', epochs=30, iterations=300, optimizer='AdamW', plots=False, save=False, val=False)
+
+model = YOLO('yolov8n.pt')
+model.tune(data='./data/kielce_university_of_technology/data.yaml', epochs=30, iterations=300, optimizer='AdamW', plots=False, save=False, val=False)
+
+model = YOLO('yolov8n.pt')
+model.tune(data='./data/toggenburg_alpaca_ranch/data.yaml', epochs=30, iterations=300, optimizer='AdamW', plots=False, save=False, val=False)
+
+fasterrcnn_resnet50_fpn.fasterrcnn_resnet50_fpn(TRAIN_ANNOTATIONS="./data/duomo/train/xml",
+                                                TRAIN_IMAGES="./data/duomo/train/images",
+                                                VAL_IMAGES="./data/duomo/valid/images",
+                                                VAL_ANNOTATIONS="./data/duomo/valid/xml",
+                                                IMG_WIDTH=640,
+                                                EPOCHS=0,
+                                                BATCH_SIZE=7,
+                                                num_classes=2,
+                                                label_to_index={'person': 1, 'no_object': 0},
+                                                PRE_TRAINED_BOOL=True)
+
+fasterrcnn_resnet50_fpn.fasterrcnn_resnet50_fpn(TRAIN_ANNOTATIONS="./data/hadji_dimitar_square/train/xml",
+                                                TRAIN_IMAGES="./data/hadji_dimitar_square/train/images",
+                                                VAL_IMAGES="./data/hadji_dimitar_square/valid/images",
+                                                VAL_ANNOTATIONS="./data/hadji_dimitar_square/valid/xml",
+                                                IMG_WIDTH=320,
+                                                EPOCHS=0,
+                                                BATCH_SIZE=7,
+                                                num_classes=2,
+                                                label_to_index={'person': 1, 'no_object': 0},
+                                                PRE_TRAINED_BOOL=True)
+
+fasterrcnn_resnet50_fpn.fasterrcnn_resnet50_fpn(TRAIN_ANNOTATIONS="./data/keskvaljak/train/xml",
+                                                TRAIN_IMAGES="./data/keskvaljak/train/images",
+                                                VAL_IMAGES="./data/keskvaljak/valid/images",
+                                                VAL_ANNOTATIONS="./data/keskvaljak/valid/xml",
+                                                IMG_WIDTH=320,
+                                                EPOCHS=0,
+                                                BATCH_SIZE=7,
+                                                num_classes=4,
+                                                label_to_index={'person': 3, 'car': 2, 'bus': 1, 'no_object': 0},
+                                                PRE_TRAINED_BOOL=True)
+
+fasterrcnn_resnet50_fpn.fasterrcnn_resnet50_fpn(TRAIN_ANNOTATIONS="./data/kielce_university_of_technology/train/xml",
+                                                TRAIN_IMAGES="./data/kielce_university_of_technology/train/images",
+                                                VAL_IMAGES="./data/kielce_university_of_technology/valid/images",
+                                                VAL_ANNOTATIONS="./data/kielce_university_of_technology/valid/xml",
+                                                IMG_WIDTH=320,
+                                                EPOCHS=0,
+                                                BATCH_SIZE=7,
+                                                num_classes=3,
+                                                label_to_index={'car': 2, 'bus': 1, 'no_object': 0},
+                                                PRE_TRAINED_BOOL=True)
+
+fasterrcnn_resnet50_fpn.fasterrcnn_resnet50_fpn(TRAIN_ANNOTATIONS="./data/toggenburg_alpaca_ranch/train/xml",
+                                                TRAIN_IMAGES="./data/toggenburg_alpaca_ranch/train/images",
+                                                VAL_IMAGES="./data/toggenburg_alpaca_ranch/valid/images",
+                                                VAL_ANNOTATIONS="./data/toggenburg_alpaca_ranch/valid/xml",
+                                                IMG_WIDTH=480,
+                                                EPOCHS=0,
+                                                BATCH_SIZE=7,
+                                                num_classes=3,
+                                                label_to_index={'sheep': 2, 'car': 1, 'no_object': 0},
+                                                PRE_TRAINED_BOOL=True)
+
+maskrcnn_resnet50_fpn.maskrcnn_resnet50_fpn(TRAIN_ANNOTATIONS="./data/duomo/train/xml",
+                                                TRAIN_IMAGES="./data/duomo/train/images",
+                                                VAL_IMAGES="./data/duomo/valid/images",
+                                                VAL_ANNOTATIONS="./data/duomo/valid/xml",
+                                                IMG_WIDTH=640,
+                                                EPOCHS=0,
+                                                BATCH_SIZE=7,
+                                                num_classes=2,
+                                                label_to_index={'person': 1, 'no_object': 0},
+                                                PRE_TRAINED_BOOL=True)
+
+maskrcnn_resnet50_fpn.maskrcnn_resnet50_fpn(TRAIN_ANNOTATIONS="./data/hadji_dimitar_square/train/xml",
+                                                TRAIN_IMAGES="./data/hadji_dimitar_square/train/images",
+                                                VAL_IMAGES="./data/hadji_dimitar_square/valid/images",
+                                                VAL_ANNOTATIONS="./data/hadji_dimitar_square/valid/xml",
+                                                IMG_WIDTH=320,
+                                                EPOCHS=0,
+                                                BATCH_SIZE=7,
+                                                num_classes=2,
+                                                label_to_index={'person': 1, 'no_object': 0},
+                                                PRE_TRAINED_BOOL=True)
+
+maskrcnn_resnet50_fpn.maskrcnn_resnet50_fpn(TRAIN_ANNOTATIONS="./data/keskvaljak/train/xml",
+                                                TRAIN_IMAGES="./data/keskvaljak/train/images",
+                                                VAL_IMAGES="./data/keskvaljak/valid/images",
+                                                VAL_ANNOTATIONS="./data/keskvaljak/valid/xml",
+                                                IMG_WIDTH=320,
+                                                EPOCHS=0,
+                                                BATCH_SIZE=7,
+                                                num_classes=4,
+                                                label_to_index={'person': 3, 'car': 2, 'bus': 1, 'no_object': 0},
+                                                PRE_TRAINED_BOOL=True)
+
+maskrcnn_resnet50_fpn.maskrcnn_resnet50_fpn(TRAIN_ANNOTATIONS="./data/kielce_university_of_technology/train/xml",
+                                                TRAIN_IMAGES="./data/kielce_university_of_technology/train/images",
+                                                VAL_IMAGES="./data/kielce_university_of_technology/valid/images",
+                                                VAL_ANNOTATIONS="./data/kielce_university_of_technology/valid/xml",
+                                                IMG_WIDTH=320,
+                                                EPOCHS=0,
+                                                BATCH_SIZE=7,
+                                                num_classes=3,
+                                                label_to_index={'car': 2, 'bus': 1, 'no_object': 0},
+                                                PRE_TRAINED_BOOL=True)
+
+maskrcnn_resnet50_fpn.maskrcnn_resnet50_fpn(TRAIN_ANNOTATIONS="./data/toggenburg_alpaca_ranch/train/xml",
+                                                TRAIN_IMAGES="./data/toggenburg_alpaca_ranch/train/images",
+                                                VAL_IMAGES="./data/toggenburg_alpaca_ranch/valid/images",
+                                                VAL_ANNOTATIONS="./data/toggenburg_alpaca_ranch/valid/xml",
+                                                IMG_WIDTH=480,
+                                                EPOCHS=0,
+                                                BATCH_SIZE=7,
+                                                num_classes=3,
+                                                label_to_index={'sheep': 2, 'car': 1, 'no_object': 0},
+                                                PRE_TRAINED_BOOL=True)
+
+retinanet_resnet50_fpn.retinanet_resnet50_fpn(TRAIN_ANNOTATIONS="./data/duomo/train/xml",
+                                                TRAIN_IMAGES="./data/duomo/train/images",
+                                                VAL_IMAGES="./data/duomo/valid/images",
+                                                VAL_ANNOTATIONS="./data/duomo/valid/xml",
+                                                IMG_WIDTH=640,
+                                                EPOCHS=0,
+                                                BATCH_SIZE=7,
+                                                num_classes=2,
+                                                label_to_index={'person': 1, 'no_object': 0},
+                                                PRE_TRAINED_BOOL=True)
+
+retinanet_resnet50_fpn.retinanet_resnet50_fpn(TRAIN_ANNOTATIONS="./data/hadji_dimitar_square/train/xml",
+                                                TRAIN_IMAGES="./data/hadji_dimitar_square/train/images",
+                                                VAL_IMAGES="./data/hadji_dimitar_square/valid/images",
+                                                VAL_ANNOTATIONS="./data/hadji_dimitar_square/valid/xml",
+                                                IMG_WIDTH=320,
+                                                EPOCHS=0,
+                                                BATCH_SIZE=7,
+                                                num_classes=2,
+                                                label_to_index={'person': 1, 'no_object': 0},
+                                                PRE_TRAINED_BOOL=True)
+
+retinanet_resnet50_fpn.retinanet_resnet50_fpn(TRAIN_ANNOTATIONS="./data/keskvaljak/train/xml",
+                                                TRAIN_IMAGES="./data/keskvaljak/train/images",
+                                                VAL_IMAGES="./data/keskvaljak/valid/images",
+                                                VAL_ANNOTATIONS="./data/keskvaljak/valid/xml",
+                                                IMG_WIDTH=320,
+                                                EPOCHS=0,
+                                                BATCH_SIZE=7,
+                                                num_classes=4,
+                                                label_to_index={'person': 3, 'car': 2, 'bus': 1, 'no_object': 0},
+                                                PRE_TRAINED_BOOL=True)
+
+retinanet_resnet50_fpn.retinanet_resnet50_fpn(TRAIN_ANNOTATIONS="./data/kielce_university_of_technology/train/xml",
+                                                TRAIN_IMAGES="./data/kielce_university_of_technology/train/images",
+                                                VAL_IMAGES="./data/kielce_university_of_technology/valid/images",
+                                                VAL_ANNOTATIONS="./data/kielce_university_of_technology/valid/xml",
+                                                IMG_WIDTH=320,
+                                                EPOCHS=0,
+                                                BATCH_SIZE=7,
+                                                num_classes=3,
+                                                label_to_index={'car': 2, 'bus': 1, 'no_object': 0},
+                                                PRE_TRAINED_BOOL=True)
+
+retinanet_resnet50_fpn.retinanet_resnet50_fpn(TRAIN_ANNOTATIONS="./data/toggenburg_alpaca_ranch/train/xml",
+                                                TRAIN_IMAGES="./data/toggenburg_alpaca_ranch/train/images",
+                                                VAL_IMAGES="./data/toggenburg_alpaca_ranch/valid/images",
+                                                VAL_ANNOTATIONS="./data/toggenburg_alpaca_ranch/valid/xml",
+                                                IMG_WIDTH=480,
+                                                EPOCHS=0,
+                                                BATCH_SIZE=7,
+                                                num_classes=3,
+                                                label_to_index={'sheep': 2, 'car': 1, 'no_object': 0},
+                                                PRE_TRAINED_BOOL=True)
 fasterrcnn_resnet50_fpn.fasterrcnn_resnet50_fpn(TRAIN_ANNOTATIONS="./data/duomo/train/xml",
                                                 TRAIN_IMAGES="./data/duomo/train/images",
                                                 VAL_IMAGES="./data/duomo/valid/images",
