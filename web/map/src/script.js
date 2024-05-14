1aaa5e88e019bff7176f1aba380e74a2d3fa43ad
Liam-Weitzel <liam.weitzel@gmail.com>
2024-05-14 18:36:04 +0200

    Demo mapbox replacement using three.js

---
+++ web/map/src/script.js
@@ -0,0 +1,106 @@
+import './style.css'
+import * as THREE from 'three'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
+
+// Textures
+const loader = new THREE.TextureLoader()
+const height = loader.load('height_map.png')
+const texture = loader.load('texture.png')
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Objects
+const geometry = new THREE.PlaneBufferGeometry(3, 3, 64, 64)
+
+// Materials
+const material = new THREE.MeshStandardMaterial({
+    color: 'gray',
+    map: texture,
+    displacementMap: height,
+    displacementScale: 2
+})
+
+// Mesh
+const plane = new THREE.Mesh(geometry, material)
+plane.rotation.x = -Math.PI/2
+scene.add(plane)
+
+// Lights
+const pointLight = new THREE.PointLight(0xffffff, 2)
+pointLight.position.x = 0
+pointLight.position.y = 10
+pointLight.position.z = 0
+scene.add(pointLight)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 0
+camera.position.y = 0.8
+camera.position.z = 1
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update Orbital Controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
